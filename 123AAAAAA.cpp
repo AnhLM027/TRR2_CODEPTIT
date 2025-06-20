40e9645b890a3dd07a9d704795389218e9d19ac4
AnhLM027 <boy14acf@gmail.com>
2025-06-20 10:43:05 +0700

    src

--- 123AAAAAA.cpp
+++ 123AAAAAA.cpp
@@ -1,70 +1,84 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-#define maxn 105
-typedef long long ll;
 
-int n, s, t;
-vector<pair<int, int>> dsc;
-vector<int> parent(maxn, -1);
-int a[maxn][maxn] = {};
+int n,m,u;
+vector<set<int>> ke(1005);
+int c[105] = {};
+bool vis[105] = {};
+vector<int> ans;
 
-void Bellman_Ford() {
-    vector<int> dp(maxn, INT_MAX);
 
-    dp[s] = 0;
-
-    for(int i = 1; i <= n - 1; i++) {
-        for(auto [u, v] : dsc) {
-            if(dp[v] > dp[u] + a[u][v]) {
-                dp[v] = dp[u] + a[u][v];
-                parent[v] = u;
-            }
-        }
-    }
-
-    bool check = false;
-    for(auto [u, v] : dsc) {
-        if(dp[v] > dp[u] + a[u][v]) {
-            check = true;
-            break;
-        }
-    }
-    
-    if(check) {
-        cout << "-1" << endl;
-        return;
-    }
-    
-    if(dp[t] == INT_MAX) {
-        cout << "0" << endl;
-        return;
-    }
-
-    else cout << dp[t] << endl;
-    
-    vector<int> res;
-    do {
-        res.push_back(t);
-        t = parent[t];
-    } while(t != -1);
-    
-    reverse(res.begin(), res.end());
-    for(int x : res) cout << x << " ";
+int check(){
+	int dem=0;
+	for(int i=1;i<=n;i++){
+		dem+=c[i] & 1;
+	}
+	if(dem==0) return 1;
+	else if(dem ==2) return 2;
+	return 0;
 }
 
-int main() {
-    freopen("BN.INP", "r", stdin);
-    freopen("BN.OUT", "w", stdout);
-    cin >> n >> s >> t;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            cin >> a[i][j];
-            if(a[i][j] != 0 && a[i][j] <= 50) dsc.push_back({i, j});
-        }
-    }
+int cntDfs = 0;
+
+void dfs(int v){
+	vis[v]=true;
+    cntDfs++;
     
-    Bellman_Ford();
-    
-    return 0;
+	for(int x:ke[v]){
+		if(!vis[x]){
+			dfs(x);
+		}
+	}
 }
+int tplt(){
+    memset(vis, false, sizeof vis);
+	dfs(1);
+    if(cntDfs < n) return 0;
+	return check();
+}
+void euler(){
+	stack<int> st;
+	st.push(u);
+	while(!st.empty()){
+		int x=st.top();
+		if(!ke[x].empty()){
+			int y=*ke[x].begin();
+			st.push(y);
+			ke[x].erase(y);
+			ke[y].erase(x);
+		}
+		else {
+			st.pop();
+			ans.push_back(x);
+		}
+	}
+}
+int main(){
+
+	int t; cin>>t;
+	if(t==1){
+		cin>>n>>m;
+		for(int i=0;i<m;i++){
+			int x,y;cin>>x>>y;
+			ke[x].insert(y);
+			ke[y].insert(x);
+			c[x]++;
+			c[y]++; 
+		}
+		cout << tplt();
+	} 
+	else if(t==2){
+		cin>>n>>m>>u;
+        for(int i=0;i<m;i++){
+			int x,y;cin>>x>>y;
+			ke[x].insert(y);
+			ke[y].insert(x);
+		} 
+		euler();
+		reverse(ans.begin(),ans.end());
+		for(int x: ans) cout<<x<<" ";
+		
+	}
+ }
